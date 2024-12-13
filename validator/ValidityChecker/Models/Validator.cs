3a887e53a866a652499da3b34ab6cb6b577caabb
Joel Andersson <joel.andersson@spinit.se>
2024-12-13 15:55:57 +0100

    Add validator class

---
+++ validator/ValidityChecker/Models/Validator.cs
@@ -0,0 +1,20 @@
+﻿namespace ValidityChecker.Models;
+
+internal interface IValidatable
+{
+    bool Check();
+}
+
+internal class Validator<T> where T : IValidatable
+{
+    // this solution is not ideal because it throws an exception if the object does not implement IValidatable i.e. won't work for primitive types
+    public bool IsValid(T obj)
+    {
+        if (!(obj is IValidatable))
+        {
+            throw new ArgumentException("Object must implement IValidatable interface");
+        }
+
+        return obj != null && obj.Check();
+    }
+}
