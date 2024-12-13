847493aa2367bb3d6dae2b819a8bbd78595a7311
Joel Andersson <joel.andersson@spinit.se>
2024-12-13 13:36:24 +0100

    Add basic console app structure, logger and planning comments

---
+++ validator/ValidityChecker/Application/Application.cs
@@ -0,0 +1,37 @@
+﻿using ValidityChecker.Util;
+
+namespace ValidityChecker.Application;
+
+public interface IApplication
+{
+    void Run();
+}
+
+public class Application
+{
+    private readonly ILogger _logger;
+
+    public Application(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Run()
+    {
+        _logger.LogInfo("Application started");
+
+        Console.WriteLine("Hello, World!");
+
+        _logger.LogInfo("Application finished");
+    }
+}
+
+/*
+ * Plan:
+ * A generic class called ValidityChecker<T> (that has a single method called IsValid() that returns a boolean?)
+ * T represents the type of the object that the class will check for validity
+ * The type should implement the IValidatable interface which has a function called Check that returns a boolean
+ * ValidityChecker will call the Check function of the object and returns the result
+ * This allows ValidityChecker to check the validity of any complexity of object
+ * By default, ValidityChecker will check if input is not null
+ */
---
+++ validator/ValidityChecker/Util/Logger.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ValidityChecker.Util;
+
+public interface ILogger
+{
+    void LogDebug(string message);
+    void LogInfo(string message);
+    void LogWarning(string message);
+    void LogError(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public ConsoleLogger() {}
+
+    public void LogDebug(string message)
+    {
+        LogToConsole("DEBUG", message);
+    }
+
+    public void LogInfo(string message)
+    {
+        LogToConsole("INFO", message);
+    }
+
+    public void LogWarning(string message)
+    {
+        LogToConsole("WARNING", message);
+    }
+
+    public void LogError(string message)
+    {
+        LogToConsole("ERROR", message);
+    }
+
+    private void LogToConsole(string logLevel, string message)
+    {
+        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{logLevel}] {message}");
+    }
+}
+
+public class FileLogger : ILogger
+{
+    private readonly string _filePath;
+
+    public FileLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void LogDebug(string message)
+    {
+        LogToFile("DEBUG", message);
+    }
+
+    public void LogInfo(string message)
+    {
+        LogToFile("INFO", message);
+    }
+
+    public void LogWarning(string message)
+    {
+        LogToFile("WARNING", message);
+    }
+
+    public void LogError(string message)
+    {
+        LogToFile("ERROR", message);
+    }
+
+    private void LogToFile(string logLevel, string message)
+    {
+        var logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{logLevel}] {message}";
+        File.AppendAllText(_filePath, logMessage + Environment.NewLine);
+    }
+}
