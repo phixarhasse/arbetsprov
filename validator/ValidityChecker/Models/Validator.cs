dfff1b2db68ac397efbf9e3bb366b1dbba857b06
Joel Andersson <joel.andersson@spinit.se>
2024-12-14 18:46:26 +0100

    Move null check

--- validator/ValidityChecker/Models/Validator.cs
+++ validator/ValidityChecker/Models/Validator.cs
@@ -9,11 +9,16 @@ internal class Validator<T> where T : IValidatable
 {
     public bool IsValid(T? obj)
     {
+        if (obj is null)
+        {
+            return false;
+        }
+
         if (!(obj is IValidatable))
         {
             throw new ArgumentException("Object must implement IValidatable interface");
         }
 
-        return obj != null && obj.Check();
+        return obj.Check();
     }
 }
